f57c99129f4400246d61922741a4c8049ceeb3a2
Jim Sadler <jim@jsdevelopment.tech>
2017-06-11 12:57:26 -0400

    initial cmmit

--- Teacup-Prophecies/SignInViewController.swift
+++ Teacup-Prophecies/SignInViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SignInViewController.swift
 //  Teacup-Prophecies
 //
 //  Created by Jim Sadler on 6/11/17.
@@ -8,17 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SignInViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  
 
 
 }
