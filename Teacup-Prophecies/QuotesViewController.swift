95e749fc6e2f5207420092344cc7b2e1826943bc
Jim Sadler <jim@jsdevelopment.tech>
2017-06-11 19:14:10 -0400

    Added quotes to database and table view

---
+++ Teacup-Prophecies/QuotesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QuotesViewController.swift
+//  Teacup-Prophecies
+//
+//  Created by Jim Sadler on 6/11/17.
+//  Copyright © 2017 JS Development. All rights reserved.
+//
+
+import UIKit
+
+class QuotesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+}
