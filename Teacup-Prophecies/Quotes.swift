1ada616b8f0b268d825cb85ee6f439c434264475
Jim Sadler <jim@jsdevelopment.tech>
2017-07-04 15:39:37 -0400

    added database connection. need to add random and single data pull functionality

---
+++ Teacup-Prophecies/Quotes.swift
@@ -0,0 +1,16 @@
+//
+//  Quotes.swift
+//  Teacup-Prophecies
+//
+//  Created by Jim Sadler on 7/4/17.
+//  Copyright © 2017 JS Development. All rights reserved.
+//
+
+import Foundation
+
+
+class Quote {
+    var quote = ""
+    var uid = ""
+}
+
--- Teacup-Prophecies/QuotesViewController.swift
+++ Teacup-Prophecies/QuotesViewController.swift
@@ -7,22 +7,37 @@
 //
 
 import UIKit
+import Firebase
 
 class QuotesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var quotes : [Quote] = []
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.tableView.dataSource = self
         self.tableView.delegate = self
+        
+        Database.database().reference().child("quotes").observe(DataEventType.childAdded, with: {(snapshot) in
+            print(snapshot)
+        })
+        
         // Do any additional setup after loading the view.
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return quotes.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = UITableViewCell()
+        
+        let quote = quotes[indexPath.row]
+        
+        cell.textLabel?.text = quote.quote
+        
+        return cell
     }
     
 
