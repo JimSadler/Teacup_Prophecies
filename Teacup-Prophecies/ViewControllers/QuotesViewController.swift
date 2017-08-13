2a590933b1c21d5211a514353f31c80c827b84e1
Jim Sadler <jim@jsdevelopment.tech>
2017-08-13 13:01:33 -0400

    added background to sign in and quote controllers, also removed table view and instead replaced it with a label and button. the app now functions as intended. needs more styling, images, links, and Icon

--- Teacup-Prophecies/ViewControllers/QuotesViewController.swift
+++ Teacup-Prophecies/ViewControllers/QuotesViewController.swift
@@ -9,26 +9,15 @@
 import UIKit
 import Firebase
 
-class QuotesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class QuotesViewController: UIViewController {
     
-    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var quoteLabel: UILabel!
+    
     
     var quotes : [Quote] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.tableView.dataSource = self
-        self.tableView.delegate = self
-        
-        
-//        Database.database().reference().child("quotes/quote\(arc4random_uniform(386))").observeSingleEvent(of: DataEventType.value, with: { (snapshot) in
-//            print(snapshot)
-//
-//
-//                    let quote = Quote()
-//                    quote.quote = (snapshot.value as!NSDictionary)["quote"] as! String
-//
-//        })
         
         Database.database().reference().child("quotes").observe(DataEventType.childAdded, with: {(snapshot) in
             print(snapshot)
@@ -37,29 +26,26 @@ class QuotesViewController: UIViewController, UITableViewDataSource, UITableView
             quote.quote = (snapshot.value! as! NSDictionary)["quote"] as! String
             
             self.quotes.append(quote)
-            self.tableView.reloadData()
         })
         
-         //Do any additional setup after loading the view.
+    }
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        newQuote()
+
     }
     
     @IBAction func logoutTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
-    }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return quotes.count
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
         
-        //let quote = quotes[indexPath.row]
-        let quote =  quotes[Int(arc4random_uniform(UInt32(quotes.count) ))]
-        
-
-        cell.textLabel?.text = quote.quote
-        
-        return cell
     }
     
-
+    func newQuote(){
+        let myQuote = quotes[Int(arc4random_uniform(UInt32(quotes.count) ))]
+        quoteLabel.text =  myQuote.quote
+    }
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        newQuote()
+        
+    }
 }
